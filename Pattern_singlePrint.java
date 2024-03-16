d8e043d8a9a509e789d12f4165cd302737de1ae0
Ashutosh patel <ashutoshpatel0002@gmail.com>
2024-03-16 08:22:04 +0530

    challenge 1

---
+++ Pattern_singlePrint.java
@@ -0,0 +1,11 @@
+//challange sheet given by KG coding
+public class Pattern_singlePrint {
+    public static void main(String[] args) {
+        System.out.println("*\n* *\n* * *\n* * * *\n* * * * *");
+        System.out.println("========================================");
+        System.out.println("* * * * * *\n* * * * *\n* * *\n* *\n*");
+        System.out.println("========================================");
+        System.out.println("          *\n        * *\n      * * *\n    * * * *\n  * * * * *");
+    }
+    
+}
