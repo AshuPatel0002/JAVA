661113f961e4222c36dae4d337a4ff51ec458d6f
Ashutosh patel <ashutoshpatel0002@gmail.com>
2024-03-16 11:02:10 +0530

    arithmetic_oprator

---
+++ Arithmetic.java
@@ -0,0 +1,23 @@
+public class Arithmetic {
+    public static void main(String[] args) {
+        int a=100;
+        int b=50;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+
+
+        float x=55;
+        float y=10;
+        
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+        System.out.println(x%y);
+        
+    }
+}
