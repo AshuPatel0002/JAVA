70831b6ca98ead700a26d0bf9b94d43f79c3aa26
Ashutosh patel <ashutoshpatel0002@gmail.com>
2024-03-16 19:39:08 +0530

    IF_else

---
+++ IF_Else.java
@@ -0,0 +1,14 @@
+public class IF_Else {
+    public static void main(String[] args) {
+        boolean ismale=true;
+        String name="sonu";
+        System.out.println("after if");
+        if(ismale){
+            System.out.println("Mr. "+name);
+        }
+        else;{
+            System.out.println("Mrs. "+name);
+        }
+        System.out.println("after if");
+    }
+}
