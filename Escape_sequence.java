9bc644c1a17fb56151e8dff61dbd6f0d68efff49
Ashutosh patel <ashutoshpatel0002@gmail.com>
2024-03-16 08:08:46 +0530

    escape sequence

---
+++ Escape_sequence.java
@@ -0,0 +1,16 @@
+public class Escape_sequence {
+    public static void main(String[] args) {
+        // insert a tab in the text at this point
+        System.out.println("welcome to\tindia");
+        //insert a backspace in the text at this point
+        System.out.println("welcome to\bindia ");
+        //insert a newline in the text at this point
+        System.out.println("welcome to\nindai");
+        //insert a single quote character in the text at this point
+        System.out.println("welcome to\' 'india'");
+        //insert  a double quote character in the text at this point
+        System.out.println("welcome to\" india");
+        //inssert a backslash character in the text at this point
+        System.out.println("welcome to\\ india");
+    }
+}
