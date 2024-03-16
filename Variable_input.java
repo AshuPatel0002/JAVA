027d2f94c4b7c3698d9ecda3252233e039da0c7a
Ashutosh patel <ashutoshpatel0002@gmail.com>
2024-03-16 07:52:56 +0530

    variable declare

---
+++ Variable_input.java
@@ -0,0 +1,26 @@
+public class Variable_input {
+    public static void main(String[] args){
+        int mynum=34;
+        int mynum1;
+        mynum1=4546;
+
+
+        System.out.println(mynum);
+        System.out.println(mynum1);
+        mynum1=344;
+        System.out.println(mynum1);
+
+        boolean isvalid=true;
+        System.out.println(isvalid);
+
+        float myfloat=45.5f;
+        System.out.println(myfloat);
+
+        byte mybite=45;
+        System.out.println(mybite);
+
+        String myString="my name is ashutosh ";
+        System.out.println(myString);
+
+    }
+}
