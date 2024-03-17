c51577e4fc0a2497c7882abdb99743e8071b46e8
Ashutosh patel <ashutoshpatel0002@gmail.com>
2024-03-17 09:16:47 +0530

    challenge 3

---
+++ Challenge_3.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+public class Challenge_3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+
+       // Q1: Create a program that determines if a number is postitive ,negative or zero
+       /*  System.out.print("Please enter a number: ");
+        int number=sc.nextInt();
+        if(number>0){
+            System.out.println("number is positive");
+        }
+        else if(number==0){
+            System.out.println("number is equal to Zero");
+        }else{
+            System.out.println("number is negative");
+        }*/
+
+
+
+        //Q2: create a program that determines if a number is off or even
+     /*   System.out.print("Please enter a number: ");
+        int Num1=sc.nextInt();
+        if(Num1%2==0){
+            System.out.println("Number is even : "+Num1);
+        }else{
+            System.out.println("Number id odd : "+Num1);
+        }*/ 
+
+
+        //Q3: Create a program that determines the greatest of the three numbers
+      /* System.out.print("enter a : ");
+        int a=sc.nextInt();
+        System.out.print("enter b : ");
+        int b=sc.nextInt();
+        System.out.print("enter c : ");
+        int c=sc.nextInt();
+
+        if(a>=b && a>=c){
+            System.out.println("A is greatest all of them");
+        }else if(b>=a && b>=c){
+            System.out.println("B is greatest all of them");
+        }else{
+            System.out.println("C is greatest all of them");
+        }  */
+
+
+        //Q4: create a program that determines if a given years is a leap year
+
+       /*  System.out.print("enter year");
+        int year=sc.nextInt();
+        if(year%400==0){
+            System.out.println("year is a leap year");
+
+        }else if(year%4==0 && year%100!=0){
+            System.out.println("year is a leap year");
+        }else{
+            System.out.println("year is not a leap year");
+        }*/
+
+
+        //Q5:create a program  that calculates grades based on marks
+        //A->above 90%       B->above 75%
+        //C->above 60%       D->above 30%
+        //F->below 30%
+
+      /*   System.out.print("Please enter your marks: ");
+        int marks=sc.nextInt();
+        if(marks>90){
+            System.out.println("A");
+        }else if(marks>75){
+        System.out.println("B");
+        }else if(marks>60){
+            System.out.println("C");
+        }else if(marks>30){
+            System.out.println("D");
+
+        }else{
+            System.out.println("F");
+        } */
+
+
+
+
+    }
+}
