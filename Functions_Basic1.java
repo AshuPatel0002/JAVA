97f59f3c1573d06d78e6f1386611ea487d1b909d
Ashutosh patel <ashutoshpatel0002@gmail.com>
2024-03-17 17:30:13 +0530

    Function_basic1

---
+++ Functions_Basic1.java
@@ -0,0 +1,16 @@
+public class Functions_Basic1 {
+    public static void main(String[] args) {
+        System.out.println("hello ");
+        greeting();
+        bhopal();
+        System.out.println("done");
+
+    }
+    public static void greeting(){
+        System.out.println("welcome to java");
+    }
+
+    public static void bhopal(){
+        System.out.println("welcome to bhopal");
+    }
+}
