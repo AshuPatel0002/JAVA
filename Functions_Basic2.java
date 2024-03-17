c0a4ea83cbac4eed61108e749b7487d45470d061
Ashutosh patel <ashutoshpatel0002@gmail.com>
2024-03-17 17:39:46 +0530

    Fuction_basic 2

---
+++ Functions_Basic2.java
@@ -0,0 +1,32 @@
+public class Functions_Basic2 {
+    public static void main(String[] args) {
+        Firstpattern();Secondpattern();thirdPattern();
+    }
+
+    public static void Firstpattern(){
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        System.out.println("* * * * *");
+        System.out.println("============");
+    }
+
+    public static void Secondpattern(){
+        System.out.println("* * * * *");
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*");
+        System.out.println("===========");
+    }
+
+
+    public static void thirdPattern(){
+        System.out.println("           *");
+        System.out.println("         * *");
+        System.out.println("       * * *");
+        System.out.println("     * * * *");
+        System.out.println("   * * * * *");
+    }
+}
