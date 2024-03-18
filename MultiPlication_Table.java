e69bb0cdcf2f1f6a64f3d934d637435aca7fe016
Ashutosh patel <ashutoshpatel0002@gmail.com>
2024-03-18 10:12:04 +0530

    table of any number

---
+++ MultiPlication_Table.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class MultiPlication_Table {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("mulpcication table number: ");
+        int num=input.nextInt();
+        for(int i=1;i<=10;i++){
+            int result=num*i;
+            System.out.println(num+" * "+i+" = "+num*i);
+           
+        }
+
+       
+    }
+       
+        
+    }
+
+
+
+   
