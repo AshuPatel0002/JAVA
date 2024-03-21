788139435f681819eb263cf8b85674f17ed1812b
Ashutosh patel <ashutoshpatel0002@gmail.com>
2024-03-21 11:07:43 +0530

    Reverse array

---
+++ Reverse_array.java
@@ -0,0 +1,9 @@
+public class Reverse_Array {
+    public static void main(String[] args) {
+        int [] arr={34,45,54,32,23,45,567,67};
+
+        for(int i=arr.length-1;i>=0;i--){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
